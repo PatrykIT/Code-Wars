a006866ad4f4649dad76a501e330e2cbd5bdb7ab
Norbert Cyrklaff <the.last.of.us.08.08@gmail.com>
2017-03-31 16:17:31 +0200

    Implementation in the making.

--- Constructing_a_Car/car.cpp
+++ Constructing_a_Car/car.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 
+/* https://www.codewars.com/kata/constructing-a-car-number-1-engine-and-fuel-tank/train/cpp */
 
 class ICar
 {
@@ -41,6 +42,8 @@ public:
 
     virtual void Refuel(double liters) = 0;
 
+    virtual bool Check_if_is_On_Reserve() = 0;
+
 protected:
 
     double fillLevel = 0.0;
@@ -67,23 +70,32 @@ protected:
 class Engine : public IEngine
 {
     friend class Car;
+
+    Engine() { isRunning = false; }
     virtual void Consume(double liters) { }
 
-    virtual void Start() { }
+    virtual void Start() { isRunning = true; }
 
-    virtual void Stop()  { }
+    virtual void Stop()  { isRunning = false; }
 };
 
 class FuelTank : public IFuelTank
 {
+    friend class Car;
+    FuelTank() { fillLevel = 20.0; isOnReserve = false; isComplete = false;}
+
     virtual void Consume(double liters) { }
 
     virtual void Refuel(double liters) { }
+
+    virtual bool Check_if_is_On_Reserve() { return fillLevel < 5.0; }
+
+    double maximum_size_of_tank = 60.0;
 };
 
 class FuelTankDisplay : public IFuelTankDisplay
 {
-
+    friend class Car;
 };
 
 class Car : public ICar
@@ -96,13 +108,23 @@ public:
         engineIsRunning = true;
     }
 
-    virtual void EngineStop() { engineIsRunning = false; }
-    virtual void Refuel(double liters) { }
-    virtual void RunningIdle() { /* The fuel consumption in running idle is 0.0003 liter/second. */ }
+    virtual void EngineStop()
+    {
+        engine->Stop();
+        engineIsRunning = false;
+    }
+
+    virtual void Refuel(double liters)
+    {
+        fuel_tank.fillLevel += liters;
+    }
+    virtual void RunningIdle() { /* The fuel consumption in running idle is 0.0003 liter/second. */   }
 
     //Engine engine;
     std::unique_ptr<Engine> engine;
 
     FuelTank fuel_tank;
     FuelTankDisplay fuel_tank_display;
+
+    double seconds_consumed = 0;
 };
